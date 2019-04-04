b8df200beddb2f826821b594b02aef8ecba1995d
dave.shepard <dave.shepard@servicenow.com>
2019-04-03 22:53:56 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import store from 'store2'
+import './App.css'
+import questions from './questions.json'
+
+function randomQuestion() {
+  let lastQuestions = store('lastQuestions') || []
+  let questionSet = questions.filter(question => !lastQuestions.includes(question))
+
+  if (!questionSet.length) {
+    questionSet = questions
+    lastQuestions = []
+  }
+  
+  const question = sample(questionSet)
+
+  store('lastQuestions', [...lastQuestions, question])
+
+  return question
+}
+
+function sample(arr) {
+  return arr[Math.ceil(Math.random() * (arr.length - 1))]
+}
 
 class App extends Component {
+  static getDerivedStateFromProps(state) {
+    if (!state.question) {
+      return {
+        question: randomQuestion()
+      }
+    }
+
+    return null
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', (event) => {
+      if (event.keyCode === 32) {
+        this.setState({
+          question: randomQuestion()
+        })
+      }
+    })
+  }
+
   render() {
+    const { question } = this.state
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Ice Breaker Question:</h1>
         </header>
+        <section>
+          {question}
+        </section>
       </div>
     );
   }
